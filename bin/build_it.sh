0f67c0ed4c796107173856dace87902c323e650f
Nick Whitelegg <nickw4426@gmail.com>
2018-01-06 12:33:52 +0000

    Mavenised xni06/JCoord

--- bin/build_it.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd ../src
-javac -target 1.6 -source 1.6 uk/me/jstott/jcoord/Test.java
-javac -target 1.6 -cp .:../lib/junit-4.12.jar -source 1.6 uk/me/jstott/jcoord/junit/AllTests.java
-cd ../bin
